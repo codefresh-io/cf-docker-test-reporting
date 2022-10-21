8284feb1680df590a64e8d6e039cda0ce4718b99
Vadim Kharin <vadim.kharin@codefresh.io>
2022-10-21 15:32:53 +0300

    CR-13431 add CF_API_RETRIES

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,24 @@
 const rp = require('request-promise');
 
 class CodefreshAPI {
+
+    static async sendRequest(config, opts) {
+        if (config.env.retriesForCodefreshAPI) {
+            let error;
+            for (let i = 0; i < config.env.retriesForCodefreshAPI; i += 1) {
+                try {
+                    // eslint-disable-next-line no-await-in-loop
+                    const result = await rp(opts);
+                    return result;
+                } catch (e) {
+                    error = e;
+                }
+            }
+            throw error;
+        }
+        return rp(opts);
+    }
+
     static async createAnnotation({ config, value }) {
         // this query use proxy ability to replace :account_id to currentAccountID
         // this ability use because we don`t know accountId
@@ -19,7 +37,7 @@ class CodefreshAPI {
             json: true,
         };
 
-        return rp(createAnnotationOpts);
+        return this.sendRequest(config, createAnnotationOpts);
     }
 
     static async getProcessById({ id, config }) {
@@ -33,7 +51,7 @@ class CodefreshAPI {
         let process;
 
         try {
-            const processRes = await rp(opts);
+            const processRes = await this.sendRequest(config, opts);
             process = JSON.parse(processRes);
         } catch (e) {
             throw new Error('Error during getting process info');
